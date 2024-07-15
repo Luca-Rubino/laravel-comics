9bfa9dbeb2ac1dd8396f8810aa0fda1ab791f3bf
Luca-Rubino <lucasrubino92@gmail.com>
2024-07-15 20:05:54 +0200

    inserire in pagina index i componenti

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -9,53 +9,12 @@
 </head>
 
 <body>
+<h1>Home</h1>
 
-    <header>
-        <div class="container">
-            <section class="image">
-                <img src="../assets/img/dc-logo.png" alt="dc-logo">
-            </section>
-            <section class="list-title">
+@include('component.header')
+@include('component.main')
+@include('component.footer')
 
-                <ul>
-                    <li v-for="(title,index) in headerLink" :key="title.id">title</li>
-                </ul>
-
-            </section>
-        </div>
-        <div class="jumbotron"></div>
-    </header>
-
-    <main>
-        <section class="container">
-            <article>
-                <h1> Current Series</h1>
-                <div class="comics">
-                    <CardDcComics v-for="(comic, index) in comicSeries" :key="index" :singleComic="comic" />
-                </div>
-                <button>Load More</button>
-            </article>
-        </section>
-    </main>
-
-    <footer>
-        <section class="d-flex">
-
-            <article class="d-flex">
-                <button>sing-up now!</button>
-            </article>
-
-            <article class="d-flex">
-                <h2>follow us</h2>
-                <img src="../img/footer-facebook.png">
-                <img src="../img/footer-twitter.png">
-                <img src="../img/footer-youtube.png">
-                <img src="../img/footer-pinterest.png">
-                <img src="../img/footer-periscope.png">
-            </article>
-
-        </section>
-    </footer>
 
 </body>
 
