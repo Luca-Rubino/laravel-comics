fbe8390aaee6bb925de2232bd7505f6e0011ef90
Luca-Rubino <lucasrubino92@gmail.com>
2024-07-16 09:30:27 +0200

    inserire dati nella route home

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,69 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
-});
+    $headerLinks = [
+        "id" => "1",
+        "title" => "characters",
+        "active" => "false",
+        "link" => "#" ,
+    
+        "id" => "2",
+        "title" => "comics",
+        "active" => "true",
+        "link" => "#" ,
+    
+        "id" => "3",
+        "title" => "movies",
+        "active" => "false",
+        "link" => "#" ,
+    
+        "id" => "4",
+        "title" => "tv",
+        "active" => "false",
+        "link" => "#",
+    
+        "id" => "5",
+        "title" => "games",
+        "active" => "false",
+        "link" => "#",
+    
+        "id" => "6",
+        "title" => "collectibles",
+        "active" => "false",
+        "link" => "#",
+    
+        "id" => "7",
+        "title" => "videos",
+        "active" => "false",
+        "link" => "#",
+    
+        "id" => "8",
+        "title" => "fan",
+        "active" => "false",
+        "link" => "#",
+    
+        "id" => "9",
+        "title" => "news",
+        "active" => "false",
+        "link" => "#",
+    
+        "id" => "10",
+        "title" => "shop",
+        "active" => "false",
+        "link" => "#",
+    ];
+
+    $comics = [
+        "immagine" => "../../src/assets/img/buy-comics-digital-comics.png",
+        "titolo" => "digital comics",
+        "immagine" => "../../src/assets/img/buy-comics-merchandise.png",
+        "titolo" => "merchandise",
+        "immagine" => "../../src/assets/img/buy-comics-subscriptions.png",
+        "titolo" => "subscription",
+        "immagine" => ".../../src/assets/img/buy-comics-shop-locator.png",
+        "titolo" => "comics shop locator",
+        "immagine" => "../../src/assets/buy-dc-power-visa.svg",
+        "titolo" => "dc power visa",
+    ];
+    return view('index', $comics, $headerLinks);
+})->name('home');
