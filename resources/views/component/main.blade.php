ddbcee92275905704b5eeab4b2dd1d98a0541901
Luca-Rubino <lucasrubino92@gmail.com>
2024-07-16 17:04:38 +0200

    Rimuovere righe vuote nel main

--- resources/views/component/main.blade.php
+++ resources/views/component/main.blade.php
@@ -1,15 +1,14 @@
 <main>
     <section class="container">
         <article>
-            <h1> Current Series</h1>
+            <h1>Current Series</h1>
             <div class="comics">
-                <article>
-                    @foreach($singleComics as $singleComic)
-                        <img src="{{$singleComic['thumb']}}" alt="{{$singleComic['series']}}">
-                        <h4>{{$singleComic['series']}}</h4>    
-                    @endforeach
-                    
-                </article>
+                @foreach($singleComics as $singleComic)
+                    <article>
+                            <img src="{{$singleComic['thumb']}}" alt="{{$singleComic['series']}}">
+                            <h4>{{$singleComic['series']}}</h4>  
+                    </article>
+                @endforeach
             </div>
             <button>Load More</button>
         </article>
